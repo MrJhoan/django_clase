d03f39dd7d32fb74764916d0f27a02af36e1e117
MrJhoan <jhoansegare2002@gmail.com>
2022-09-08 06:42:38 -0500

    okk

--- libreria/libreria/settings.py
+++ libreria/libreria/settings.py
@@ -52,7 +52,10 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware'
 ]
+
+
 
 ROOT_URLCONF = 'libreria.urls'
 
