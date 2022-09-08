90b84be844442d57e25e6c13dcfe212a6a9c7a7a
MrJhoan <jhoansegare2002@gmail.com>
2022-09-08 06:36:22 -0500

    ok

--- libreria/libreria/settings.py
+++ libreria/libreria/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-+^kequrtm_s0ck&^n@*^7lp&1718oyv7j&^ko$tvd_lc1&dmg2
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [*]
 
 
 # Application definition
